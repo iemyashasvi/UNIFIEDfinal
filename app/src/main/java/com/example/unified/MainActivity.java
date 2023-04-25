e0cfa380873a901832543eeea70d2058f209fb03
Yashasvi Kapil <varu915@gmail.com>
2023-04-25 08:40:51 +0530

    commit

--- app/src/main/java/com/example/unified/MainActivity.java
+++ app/src/main/java/com/example/unified/MainActivity.java
@@ -2,19 +2,23 @@ package com.example.unified;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button notestemp;
+    Button notestemp,expensetemp;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         notestemp=findViewById(R.id.notesbuttontemp);
         notestemp.setOnClickListener(v->startActivity(new Intent(MainActivity.this,notesactivity.class)));
+
+
 
     }
 }
